57339633135b4a512dff70b293af0477834044d8
Senio Caires <seniocaires@gmail.com>
2017-12-17 18:32:06 -0200

    Random commit
    O código decide aleatoriamente se irá efetivar as alterações ou não.

--- Dockerfile
+++ Dockerfile
@@ -4,11 +4,12 @@ LABEL maintainer="seniocaires@gmail.com"
 
 COPY . /app
 
-RUN apt-get update && apt-get install -y --no-install-recommends git cron \
+RUN apt-get update && apt-get install -y --no-install-recommends git ca-certificates cron \
     && mv /app/cron/crontab /etc/cron.d/github-contributions-cron \
     && chmod 0644 /etc/cron.d/github-contributions-cron \
     && /usr/bin/crontab /etc/cron.d/github-contributions-cron \
     && chmod +x /app/entrypoint.sh \
+    && chmod +x /app/shell/github-contribution.sh \
     && rm -rf /var/lib/apt/lists/*
 
 ENTRYPOINT ["sh", "/app/entrypoint.sh"]
--- shell/github-contribution.sh
+++ shell/github-contribution.sh
@@ -1,25 +1,33 @@
 #!bin/bash
 
-echo "Clonando o repositorio "${GITHUB_RATING_REPOSITORY_NAME}"..."
-rm -rf /tmp/rating
-url_repositorio="https://"${GITHUB_USER_NAME}:${GITHUB_PASSWORD}@github.com/${GITHUB_USER_NAME}/${GITHUB_RATING_REPOSITORY_NAME}".git"
-cd /tmp
-git clone ${url_repositorio} rating
-echo "Repositorio clonado em /tmp/rating!"
+if [ $((RANDOM % 2)) = 0 ] ; then
 
-echo "Alterando o arquivo /tmp/rating/shell/string-random ... "
-string_random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
-echo ${string_random} > /tmp/rating/shell/string-random
-echo "Arquivo alterado!"
+  echo "Clonando o repositorio "${GITHUB_RATING_REPOSITORY_NAME}"..."
+  rm -rf /tmp/rating
+  url_repositorio="https://"${GITHUB_USER_NAME}:${GITHUB_PASSWORD}@github.com/${GITHUB_USER_NAME}/${GITHUB_RATING_REPOSITORY_NAME}".git"
+  cd /tmp
+  git clone ${url_repositorio} rating
+  echo "Repositorio clonado em /tmp/rating!"
 
-cd /tmp/rating
-echo "Configurando git (autor) ..."
-git config user.name ${GIT_CONFIG_USER_NAME}
-git config user.email ${GIT_CONFIG_USER_EMAIL}
-echo "Adicionando para Stage Area ..."
-git add .
-echo "Commit ..."
-git commit -m "Alteracao do arquivo string-random"
-echo "Push ..."
-git push ${url_repositorio} --all
+  echo "Alterando o arquivo /tmp/rating/shell/string-random ... "
+  string_random=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
+  echo ${string_random} > /tmp/rating/shell/string-random
+  echo "Arquivo alterado!"
+
+  cd /tmp/rating
+  echo "Configurando git (autor) ..."
+  git config user.name ${GIT_CONFIG_USER_NAME}
+  git config user.email ${GIT_CONFIG_USER_EMAIL}
+  echo "Adicionando para Stage Area ..."
+  git add .
+  echo "Commit ..."
+  git commit -m "Alteracao do arquivo string-random"
+  echo "Push ..."
+  git push ${url_repositorio} --all
+
+else
+
+  echo "Random nao atendido."
+
+fi
 
